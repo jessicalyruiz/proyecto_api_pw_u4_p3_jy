25280b727a603f4cf68d2e194c655e072d561101
jessicalyruiz <jessicalyr@gmail.com>
2023-02-22 19:20:58 -0500

    taller33

---
+++ src/main/java/com/example/demo/ProyectoApiPwU4P3JyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoApiPwU4P3JyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoApiPwU4P3JyApplication.class, args);
+	}
+
+}
